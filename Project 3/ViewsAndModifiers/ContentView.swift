3be7a726b3d2a466e0f8b818ea4f6cefe65a7de1
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-01-24 23:32:34 +0100

    text view modifiers

--- Project 3/ViewsAndModifiers/ContentView.swift
+++ Project 3/ViewsAndModifiers/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Welcome")
+            .font(.title)
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
