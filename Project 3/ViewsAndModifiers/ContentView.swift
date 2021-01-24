0d6b3203527eba5f449df6d67f5c6fc13109a84a
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-01-25 00:42:31 +0100

    Custom modifiers example with (re)building view structure.

--- Project 3/ViewsAndModifiers/ContentView.swift
+++ Project 3/ViewsAndModifiers/ContentView.swift
@@ -99,12 +99,64 @@ struct CapsuleText: View {
     }
 }
 
-struct ContentView: View {
+struct CustomViewContentView: View {
     var body: some View {
         VStack(spacing: 10) {
             CapsuleText(text: "First")
                 .foregroundColor(.yellow)
             CapsuleText(text: "Second")
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .center) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+                .opacity(0.7)
+                .rotationEffect(.init(degrees: -15))
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.gray.edgesIgnoringSafeArea(.all)
+            
+            Text("Hello world")
+                .titleStyle()
+                .watermarked(with: "http://www.octotap.com")
         }
     }
 }
