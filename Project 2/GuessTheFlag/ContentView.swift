821fa63c2fbc352e862b24d9fc70c6e1bb08b4e5
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-01-24 11:50:40 +0100

    adding HStack,VStack, ZStack examples

--- Project 2/GuessTheFlag/ContentView.swift
+++ Project 2/GuessTheFlag/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 20) {
+            VStack(alignment: .center, spacing: 10) {
+                Text("Placeholder 1")
+                Spacer()
+                Text("Placeholder 2")
+                Spacer()
+                Text("Placeholder 3")
+            }
+            HStack(alignment: .center, spacing: 10) {
+                Text("Placeholder 1 - super long label")
+                Text("Placeholder 2")
+                Text("Placeholder 3")
+            }
+            ZStack() {
+                Color.red
+                Text(" -      - ")
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                Text("Top")
+            }
+            
+        }
     }
 }
 
