e93b3cb9dc82e0284cdef73831645988058fe52d
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-02-03 00:06:43 +0100

    Implementing examples of DatePicker, Stepper. Adding trained ML model

--- Project 4/BetterRestHWS/ContentView.swift
+++ Project 4/BetterRestHWS/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var hours: Double = 8
+    @State private var selectedDate: Date = Date()
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Stepper(value: $hours, in: 4...20, step: 0.25) {
+                Text("\(hours, specifier: "%g")")
+                    .padding()
+            }
             .padding()
+            
+            Form {
+                DatePicker("Select date", selection: $selectedDate, in: Date()..., displayedComponents: .hourAndMinute)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .labelsHidden()
+                
+            }
+            
+        }
+        
     }
 }
 
