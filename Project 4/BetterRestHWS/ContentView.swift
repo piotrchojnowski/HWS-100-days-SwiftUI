3fbcb14d33903ad4be84975079c03e124bea9125
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-02-20 22:59:30 +0100

    Project 4: adding default wake date, moving to better UI style. making use of Form and grouped Vstacks to have grouped by sections.

--- Project 4/BetterRestHWS/ContentView.swift
+++ Project 4/BetterRestHWS/ContentView.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var hours: Double = 8
     @State private var selectedDate: Date = Date()
     
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
@@ -19,37 +20,58 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-
-                Text("Desired amount of sleep")
-                    .font(.headline)
-
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                        .padding(.bottom, 8)
 
-                Stepper(value: $coffeeAmount, in: 1...10) {
-                    if coffeeAmount == 1 {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                        .padding(.bottom, 8)
+                    
+                    Stepper(value: $coffeeAmount, in: 1...10) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
                     }
                 }
             }
             .navigationBarTitle("BetterRest")
             .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
+                HStack {
+                    Image(systemName: "clock.fill")
+                        .foregroundColor(Color(UIColor.systemGray))
+                    Button(action: calculateBedtime) {
+                        Text("Calculate")
+                    }
                 }
             )
             .alert(isPresented: $showingAlert) {
@@ -59,17 +81,18 @@ struct ContentView: View {
     }
     
     private func calculateBedtime() {
-        let model = SleepCalculatorModel()
-        
-        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-        let hour = (components.hour ?? 0) * 60 * 60
-        let minute = (components.minute ?? 0) * 60
-        
         do {
+            let model = try SleepCalculatorModel(configuration: MLModelConfiguration())
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
 
             let sleepTime = wakeUp - prediction.actualSleep
             let formatter = DateFormatter()
+            formatter.dateStyle = .short
             formatter.timeStyle = .short
 
             alertMessage = formatter.string(from: sleepTime)
@@ -87,5 +110,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(nil)
     }
 }
