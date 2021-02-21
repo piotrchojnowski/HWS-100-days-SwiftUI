d74165ace2a3172844501a1b4411dc01ef6f1d56
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-02-22 00:58:40 +0100

    Project 5: adding new project files.

---
+++ Project 5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Piotr Chojnowski on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
