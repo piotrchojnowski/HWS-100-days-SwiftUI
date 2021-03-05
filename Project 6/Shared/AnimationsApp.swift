88c560efd937c79e4496940fc50ee1474dc0c8ed
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-03-05 23:30:31 +0100

    Project-6: Project setup

---
+++ Project 6/Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by Piotr Chojnowski on 05/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
