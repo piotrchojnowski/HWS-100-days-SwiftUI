6a92c3ca37d3c6ca77bab44d0e14c451fbabc9ac
Piotr Chojnowski <p.chojnowski@octotap.com>
2021-03-29 00:32:30 +0200

    Project-8: Init.

---
+++ Project 8/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Piotr Chojnowski on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
