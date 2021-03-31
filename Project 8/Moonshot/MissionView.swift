b368c6f276996c8c1deb7db136506e9b2d60d648
Piotr Chojnowski <chesterman@gmail.com>
2021-03-31 23:07:14 +0200

    Project-8: Complete mission detail view.

--- Project 8/Moonshot/MissionView.swift
+++ Project 8/Moonshot/MissionView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let astronauts: [CrewMember]
     
     var body: some View {
         GeometryReader { geo in
@@ -21,7 +28,32 @@ struct MissionView: View {
                         .padding(.top)
                     
                     Text(self.mission.description)
+                        .minimumScaleFactor(0.5)
                         .padding()
+                    
+                    ForEach(self.astronauts, id: \.role) { crewMember in
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 92, height: 64, alignment: .center)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8).stroke(Color.secondary, lineWidth: 1))
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .font(.headline)
+                                
+                                Text(crewMember.role)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
                     
                     Spacer(minLength: 25)
                 }
@@ -29,12 +61,29 @@ struct MissionView: View {
         }
         .navigationBarTitle(Text(self.mission.displayName), displayMode: .inline)
     }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+
+        var matches = [CrewMember]()
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+
+        self.astronauts = matches
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
 
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
     }
 }
