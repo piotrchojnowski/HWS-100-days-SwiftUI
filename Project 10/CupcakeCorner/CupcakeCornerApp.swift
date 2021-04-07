6d936ff3dc412d42c3b969d1e6f4d1b49e159f3d
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 21:03:48 +0200

    Project-10: Init.

---
+++ Project 10/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Piotr Chojnowski on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
