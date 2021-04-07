f9da4998642e87e6388c5a41e3dfb82e6b6c765d
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 21:32:04 +0200

    Project-10: Sending and receiving Codable data with URLSession

--- Project 10/CupcakeCorner/ContentView.swift
+++ Project 10/CupcakeCorner/ContentView.swift
@@ -7,13 +7,60 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+
+struct Response: Codable {
+    var results: [Result]
 }
 
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            guard let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) else {
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            // we have good data – go back to the main thread
+            DispatchQueue.main.async {
+                // update our UI
+                self.results = decodedResponse.results
+            }
+
+        }.resume()
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
