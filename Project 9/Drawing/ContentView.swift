4142a57376a713e46f93332ae9ee217d9244e876
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 15:50:31 +0200

    Project-9: Special effects blurs, blending.

--- Project 9/Drawing/ContentView.swift
+++ Project 9/Drawing/ContentView.swift
@@ -192,7 +192,7 @@ struct ColorCyclingCircle: View {
     }
 }
 
-struct ContentView: View {
+struct ColorCyclingContentView: View {
     @State private var colorCycle = 0.0
 
     var body: some View {
@@ -203,6 +203,80 @@ struct ContentView: View {
             Slider(value: $colorCycle)
         }
     }
+}
+
+struct BlendModeContentView: View {
+//    var body: some View {
+//        ZStack {
+//            Image("cupcakes")
+//
+//            Rectangle()
+//                .fill(Color.green)
+//                .blendMode(.multiply)
+//        }
+//        .frame(width: 400, height: 300)
+//        .clipped()
+//    }
+    
+//    var body: some View {
+//        Image("cupcakes")
+//            .colorMultiply(.green)
+//    }
+    
+    @State private var amount: CGFloat = 0.0
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
+struct ContentView: View {
+    @State private var amount: CGFloat = 0.0
+    
+    var body: some View {
+        VStack {
+            Image("cupcakes")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 200)
+                .saturation(Double(amount))
+                .blur(radius: (1 - amount) * 25)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
