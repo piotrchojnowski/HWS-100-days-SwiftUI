bde49a10d495dfdc6a5d8ef49231d6296a915a6a
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 11:08:18 +0200

    Project-9: init.

---
+++ Project 9/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Piotr Chojnowski on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
