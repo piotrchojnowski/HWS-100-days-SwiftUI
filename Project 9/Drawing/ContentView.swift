3393e89127d15dbbdc263da5400e5044b605958a
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 13:08:55 +0200

    Project-9: Adding Shapes and conforming to a InsettableShape.

--- Project 9/Drawing/ContentView.swift
+++ Project 9/Drawing/ContentView.swift
@@ -7,10 +7,86 @@
 
 import SwiftUI
 
+struct PathContentView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .miter))
+//        .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - (rotationAdjustment * 2)
+        let modifiedEnd = endAngle - (rotationAdjustment * 2)
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2 - insetAmount,
+                    startAngle: modifiedStart,
+                    endAngle: modifiedEnd,
+                    clockwise: !clockwise)
+
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct ShapesContentView: View {
+    var body: some View {
+        VStack {
+            Triangle()
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 100, height: 100, alignment: .center)
+            
+            Divider()
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(120), clockwise: true)
+                .stroke(Color.blue, lineWidth: 10)
+                .frame(width: 200, height: 200, alignment: .center)
+        }
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                .strokeBorder(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .miter, miterLimit: 0, dash: [10, 2], dashPhase: 0))
+                .frame(width: 300, height: 300, alignment: .center)
+        }
+        
     }
 }
 
