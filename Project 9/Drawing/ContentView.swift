815158cc52f901f8e835a17ea2d28a7bd17496e6
Piotr Chojnowski <chesterman@gmail.com>
2021-04-07 15:57:35 +0200

    Project-9: Animating simple shapes with animatableData.

--- Project 9/Drawing/ContentView.swift
+++ Project 9/Drawing/ContentView.swift
@@ -257,7 +257,7 @@ struct BlendModeContentView: View {
     
 }
 
-struct ContentView: View {
+struct SaturationBlurContentView: View {
     @State private var amount: CGFloat = 0.0
     
     var body: some View {
@@ -275,6 +275,42 @@ struct ContentView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
         .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
+struct ContentView: View {
+    @State private var insetAmount: CGFloat = 50
+    
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            }
     }
     
 }
