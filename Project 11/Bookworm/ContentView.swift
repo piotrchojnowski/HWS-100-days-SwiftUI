2050f920c64b7f685258be3e4db9d0f46fd4585b
Piotr Chojnowski <chesterman@gmail.com>
2021-04-09 23:13:14 +0200

    Project-11: Book Detail view

--- Project 11/Bookworm/ContentView.swift
+++ Project 11/Bookworm/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             List {
                 ForEach(books, id: \.self) { book in
                     NavigationLink(
-                        destination: Text(book.title ?? "Unknown Title")) {
+                        destination: DetailView(book: book)) {
                         
                         EmojiRatingView(rating: book.rating)
                             .font(.title)
---
+++ Project 11/Bookworm/DetailView.swift
@@ -0,0 +1,64 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Piotr Chojnowski on 09/04/2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.secondary.opacity(0.7))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+                
+                Text(self.book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+
+                Text(self.book.review ?? "No review")
+                    .padding()
+
+                RatingView(rating: .constant(Int(self.book.rating)), label: "Rating")
+                    .font(.largeTitle)
+                    .padding(.horizontal)
+
+                Spacer()
+            }
+        }
+        .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Romance"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
