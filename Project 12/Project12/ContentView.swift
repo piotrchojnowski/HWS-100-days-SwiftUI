5edb301b2bf2ccfc5dad57b48d7be016121c9dbe
Piotr Chojnowski <chesterman@gmail.com>
2021-04-11 21:35:27 +0200

    Project-12: Filtering FetchRequest using NSPredicate

--- Project 12/Project12/ContentView.swift
+++ Project 12/Project12/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
@@ -19,27 +21,53 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: self.viewContext)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                withAnimation {
-                    do {
-                        try self.viewContext.save()
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.viewContext)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.viewContext)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.viewContext)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: self.viewContext)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? self.viewContext.save()
             }
         }
+//        VStack {
+//            List(wizards, id: \.self) { wizard in
+//                Text(wizard.name ?? "Unknown")
+//            }
+//
+//            Button("Add") {
+//                let wizard = Wizard(context: self.viewContext)
+//                wizard.name = "Harry Potter"
+//            }
+//
+//            Button("Save") {
+//                withAnimation {
+//                    do {
+//                        try self.viewContext.save()
+//                    } catch {
+//                        print(error.localizedDescription)
+//                    }
+//                }
+//            }
+//        }
+        
+        
 //        List {
 //            ForEach(items) { item in
 //                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
