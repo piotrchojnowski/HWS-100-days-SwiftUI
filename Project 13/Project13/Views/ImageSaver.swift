0a3ae1db6371185dfa2d26ca00f8c29b8c4c4804
Piotr Chojnowski <chesterman@gmail.com>
2022-09-07 23:38:41 +0200

    Project-13: Add image saver for objc photo picker saving.

---
+++ Project 13/Project13/Views/ImageSaver.swift
@@ -0,0 +1,19 @@
+//
+//  ImageSaver.swift
+//  Project13
+//
+//  Created by Piotr Chojnowski on 07/09/2022.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
--- Project 13/Project13/Views/ViewControllerInView.swift
+++ Project 13/Project13/Views/ViewControllerInView.swift
@@ -22,6 +22,15 @@ struct ViewControllerInView: View {
             Button("Select Image") {
                 showingImagePicker = true
             }
+            
+            Button("Save Image") {
+                guard let inputImage = inputImage else {
+                    return
+                }
+
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
         }
         .sheet(isPresented: $showingImagePicker) {
             ImagePicker(image: $inputImage)
