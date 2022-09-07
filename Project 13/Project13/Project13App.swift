51ac618e3f792f11de1354dfcbba80c750360724
Piotr Chojnowski <chesterman@gmail.com>
2022-09-07 21:05:16 +0200

    Project-13: Project Instafilter init.

---
+++ Project 13/Project13/Project13App.swift
@@ -0,0 +1,20 @@
+//
+//  Project13App.swift
+//  Project13
+//
+//  Created by Piotr Chojnowski on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project13App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
