2db2e0a60170743012d1024ca1d919fd5d311fcf
Piotr Chojnowski <chesterman@gmail.com>
2022-09-10 22:31:10 +0200

    Project-15: Init

---
+++ Project 15/Project15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project15
+//
+//  Created by Piotr Chojnowski on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 15/Project15/Project15App.swift
@@ -0,0 +1,17 @@
+//
+//  Project15App.swift
+//  Project15
+//
+//  Created by Piotr Chojnowski on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
