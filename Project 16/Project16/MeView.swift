1cadb56baa96b282edc6c2bc719c2a14137f6bf7
Piotr Chojnowski <chesterman@gmail.com>
2022-09-15 17:30:06 +0200

    Project-16: QR code generation

--- Project 16/Project16/MeView.swift
+++ Project 16/Project16/MeView.swift
@@ -5,11 +5,49 @@
 //  Created by Piotr Chojnowski on 14/09/2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "email@email.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your QR code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
