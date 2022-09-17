54fce3dbed47842357585a370c0cd5d880fc42f6
Piotr Chojnowski <chesterman@gmail.com>
2022-09-17 21:34:00 +0200

    Project-16: Finished project 16. adding local notifications as reminders.

--- Project 16/Project16/ProspectsView.swift
+++ Project 16/Project16/ProspectsView.swift
@@ -6,6 +6,7 @@
 //
 import CodeScanner
 import SwiftUI
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -42,6 +43,13 @@ struct ProspectsView: View {
                                 Label("Mark Contacted", systemImage: "person.crop.circle.badge.checkmark")
                             }
                             .tint(.green)
+                            
+                            Button {
+                                addNotification(for: prospect)
+                            } label: {
+                                Label("Remind Me", systemImage: "bell")
+                            }
+                            .tint(.orange)
                         }
                     }
                 }
@@ -99,6 +107,42 @@ struct ProspectsView: View {
             
         case .failure(let failure):
             print("scanning failed: \(failure.localizedDescription)")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                                content: content,
+                                                trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.badge, .alert, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Notifications request rejected. \(error?.localizedDescription ?? "-")")
+                    }
+                }
+            }
         }
     }
 }
