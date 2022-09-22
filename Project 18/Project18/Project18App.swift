5d4ca773d41ead13b0fe3944902a1e07087c92c5
Piotr Chojnowski <chesterman@gmail.com>
2022-09-22 15:43:02 +0200

    Project-18: Init

---
+++ Project 18/Project18/Project18App.swift
@@ -0,0 +1,17 @@
+//
+//  Project18App.swift
+//  Project18
+//
+//  Created by Piotr Chojnowski on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
