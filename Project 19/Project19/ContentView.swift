9d92bda8928bdfe18136e465989384fd4de451a7
Piotr Chojnowski <chesterman@gmail.com>
2022-09-26 23:01:14 +0200

    Project-19: different navigation link layouts example.

--- Project 19/Project19/ContentView.swift
+++ Project 19/Project19/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink {
+                Text("New secondary")
+            } label: {
+                Text("Hello, World!")
+            }
+            .navigationTitle("Primary")
+
+            Text("Secondary")
         }
-        .padding()
     }
 }
 
