fc2cc28bab7ec52f48f187a2960c2655d7046b72
Piotr Chojnowski <chesterman@gmail.com>
2022-09-30 23:45:18 +0200

    Project-19: Finished.

--- Project 19/Project19/ResortView.swift
+++ Project 19/Project19/ResortView.swift
@@ -13,6 +13,8 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
     
+    @EnvironmentObject var favorites: Favorites
+    
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
     
@@ -64,6 +66,16 @@ struct ResortView: View {
                     // Text version
 //                    Text(resort.facilities, format: .list(type: .and))
 //                        .padding(.vertical)
+                    
+                    Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                        if favorites.contains(resort) {
+                            favorites.remove(resort)
+                        } else {
+                            favorites.add(resort)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding()
                 }
                 .padding(.horizontal)
             }
@@ -80,5 +92,6 @@ struct ResortView: View {
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
         ResortView(resort: Resort.example)
+            .environmentObject(Favorites())
     }
 }
